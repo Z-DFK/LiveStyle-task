495d00aa1e443d1f9b22e78b155d73e48550ea72
Z-DFK <gug2005av@gmail.com>
2025-03-01 19:32:32 +0300

    Улучшение дезайна шапки

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,13 +1,13 @@
-<header class="border-b border-gray-700 py-2.5">
+<header class="border-b border-gray-300 py-2.5">
     <x-container>
         <div class="flex justify-between items-center">
             <div class="">
                 <a href=" {{route('page.home') }}" class="text-2xl">
-                    <i class='bx bxl-sketch'></i>
+                    <i class='bx bxs-cube' style='color:#00bbff'  ></i>
                 </a>
             </div>
             <nav>
-                <ul class="flex text-gray-400 text-lg gap-2">
+                <ul class="flex text-gray-400 text-lg gap-8">
                     <li class="">
                         <a href="" class="hover:text-gray-800 hover:font-semibold">Web</a>
                     </li>
@@ -15,27 +15,27 @@
                         <a href="" class="hover:text-gray-800 hover:font-semibold">Sports</a>
                     </li>
                     <li class="">
-                        <a href="" class="hover:text-gray-800 hover:font-semibold">Opinion</a>
+                        <a href="" class="hover:text-gray-800 hover:font-bold">Opinion</a>
                     </li>
                     <li class="">
-                        <a href="" class="hover:text-gray-800 hover:font-semibold">Lifestyle</a>
+                        <a href="" class="hover:text-gray-800 hover:font-bold">Lifestyle</a>
                     </li>
                     <li class="">
-                        <a href="" class="hover:text-gray-800 hover:font-semibold">Business</a>
+                        <a href="" class="hover:text-gray-800 hover:font-bold">Business</a>
                     </li>
                     <li class="">
-                        <a href="" class="hover:text-gray-800 hover:font-semibold">Culture</a>
+                        <a href="" class="hover:text-gray-800 hover:font-bold">Culture</a>
                     </li>
                     <li class="">
-                        <a href="" class="hover:text-gray-800 hover:font-semibold">Fashion</a>
+                        <a href="" class="hover:text-gray-800 hover:font-bold">Fashion</a>
                     </li>
                     <li class="">
-                        <a href="" class="hover:text-gray-800 hover:font-semibold">Tech</a>
+                        <a href="" class="hover:text-gray-800 hover:font-bold">Tech</a>
                     </li>
                 </ul>
             </nav>
             <div class="">
-                <img class="w-[50px] h-[50px] rounded-full" src="{{ asset('storage/image/prod1.jpg') }}" alt="">
+                <img class="w-[50px] h-[50px] rounded-full" src="{{ asset('storage/image/ychwzr3uqi481.png') }}" alt="">
             </div>
         </div>
     </x-container>
