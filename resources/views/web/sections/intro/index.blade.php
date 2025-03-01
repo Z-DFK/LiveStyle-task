be14cf787d40698b14f9da682fd18634c0822302
Z-DFK <gug2005av@gmail.com>
2025-03-01 19:43:48 +0300

    Улучшение дизайна меню секции

--- resources/views/web/sections/intro/index.blade.php
+++ resources/views/web/sections/intro/index.blade.php
@@ -8,51 +8,50 @@ class="w-full pt-[70px] pb-7">
             The latest and best lifestyle articles selected <br> by our editorial office
         </p>
         <div class=" flex flex-col justify-center md:grid grid-cols-4 grid-rows-7 gap-4 mt-[30px]">
-            <div class="  p-10 col-span-2 row-span-3  py-[30px] pl-[15px] border-none rounded-[10px]  w-full h-full  " style="background-image: url('{{ asset ("storage/image/coffee.png") }}');background-position: center; w-full; h-full; background-size: cover; background-repeat: no-repeat; ">
+            <div class="bg-[#6BC2A6] p-10 col-span-2 row-span-3  py-[30px] pl-[15px] border-none rounded-[10px]  w-full h-full  " style="background-image: url('{{ asset ("storage/image/coffee.png") }}');background-position: center; w-full; h-full; background-size: cover; background-repeat: no-repeat; ">
                 <span class=" text-[14px] md:text-[16px] text-white" >#000</span>
                 <h3 class=" text-[21px] md:text-[24px] text-white font-bold">Wake Up and <br>
                 Smell the Coffee</h3>
-                <button class=" mt-[50px] md:text-[#b6b6e3] bg-white px-[10px] py-[5px] md:mt-[100px]">
+                <button class=" mt-[50px] md:text-[#6BC2A6] bg-white px-[20px] py-[5px] md:mt-[100px] cursor-pointer">
                     Read for $0,99
                 </button>
             </div>
-            <div class="row-span-4 col-start-3 py-[20px] pl-[15px] border-none rounded-[10px]  w-full h-full  " style="background-image: url('{{ asset ("storage/image/dom.jpg") }}');background-position: center; w-full; h-full; background-size: cover; background-repeat: no-repeat; "">
-
+            <div class="bg-gray-700 row-span-4 col-start-3 py-[20px] pl-[15px] border-none rounded-[10px]  w-full h-full  " style="background-image: url('{{ asset ("storage/image/dom.jpg") }}');background-position: center; w-full; h-full; background-size: cover; background-repeat: no-repeat; "">
                 <span class=" text-[14px] md:text-[16px] text-white" >ARCHITECTURE</span>
                 <h3 class=" text-[21px] md:text-[24px] text-white font-bold">The Brand New NASSA Office <br>
                 Smell the Coffee</h3>
-                <button class="mt-[100px] md:text-[#b6b6e3] bg-white px-[10px] py-[5px] md:mt-[180px]">
+                <button class="mt-[100px] md:text-gray-700 bg-white px-[20px] py-[5px] md:mt-[180px] cursor-pointer">
                     Read for $0.19
                 </button>
             </div>
-            <div class="row-span-4 col-start-4 py-[20px] pl-[15px] border-none rounded-[10px]  w-full h-full  " style="background-image: url('{{ asset ("storage/image/2card.jpg") }}');background-position: center; w-full; h-full; background-size: cover; background-repeat: no-repeat; " ">
+            <div class="bg-[#C2B86B] row-span-4 col-start-4 py-[20px] pl-[15px] border-none rounded-[10px]  w-full h-full  " style="background-image: url('{{ asset ("storage/image/2card.jpg") }}');background-position: center; w-full; h-full; background-size: cover; background-repeat: no-repeat; " ">
                 <span class=" textt-[14px] md:text-[16px] text-white" >TRAVEL</span>
                 <h3 class=" text-[21px] md:text-[24px] text-white font-bold">Experience the<br>
                 Saharan Sands</h3>
-                <button class="mt-[100px] md:text-[#b6b6e3] bg-white px-[10px] py-[5px] md:mt-[210px]">
+                <button class="mt-[100px] md:text-[#C2B86B] bg-white px-[20px] py-[5px] md:mt-[210px] cursor-pointer">
                     Read for $2.29
                 </button>
             </div>
-            <div class="row-span-4 row-start-4 py-[20px] pl-[15px] border-none rounded-[10px]  w-full h-full  " style="background-image: url('{{ asset ("storage/image/zveti.jpg") }}');background-position: center; w-full; h-full; background-size: cover; background-repeat: no-repeat; "">
+            <div class="bg-[#6BABC2] row-span-4 row-start-4 py-[20px] pl-[15px] border-none rounded-[10px]  w-full h-full  " style="background-image: url('{{ asset ("storage/image/zveti.jpg") }}');background-position: center; w-full; h-full; background-size: cover; background-repeat: no-repeat; "">
                 <span class=" text-[14px] md:text-[16px] text-white" >TRAVEL</span>
                 <h3 class=" text-[21px]  md:text-[24px] text-white font-bold">9 Air-Cleaning<br>
                 Plants Your <br> Home Needs</h3>
-                <button class="mt-[100px] md:text-[#b6b6e3] bg-white px-[10px] py-[5px] md:mt-[180px]">
+                <button class="mt-[100px] md:text-[#6BABC2] bg-white px-[20px] py-[5px] md:mt-[180px] cursor-pointer">
                     Read for $0.09
                 </button>
             </div>
-            <div class="row-span-4 row-start-4 py-[20px] pl-[15px] border-none rounded-[10px]  w-full h-full  " style="background-image: url('{{ asset ("storage/image/ruka.jpg") }}');background-position: center; w-full; h-full; background-size: cover; background-repeat: no-repeat; "">
+            <div class="bg-[#C26B83] row-span-4 row-start-4 py-[20px] pl-[15px] border-none rounded-[10px]  w-full h-full  " style="background-image: url('{{ asset ("storage/image/ruka.jpg") }}');background-position: center; w-full; h-full; background-size: cover; background-repeat: no-repeat; "">
                 <span class=" text-[14px] md:text-[16px] text-white" >TRAVEL</span>
                 <h3 class="text-[21px] md:text-[24px] text-white font-bold">One Month <br>
                 Sugar Detox</h3>
-                <button class="mt-[100px] md:text-[#b6b6e3] bg-white px-[10px] py-[5px] md:mt-[210px]">
+                <button class="mt-[100px] md:text-[#C26B83] bg-white px-[20px] py-[5px] md:mt-[210px] cursor-pointer">
                     Read for $0.99
                 </button></div>
-            <div class="col-span-2 row-span-3 col-start-3 row-start-5 py-[30px] pl-[15px] border-none rounded-[10px]  w-full h-full  " style="background-image: url('{{ asset ("storage/image/dom2.jpg") }}');background-position: center; w-full; h-full; background-size: cover; background-repeat: no-repeat; "">
+            <div class="bg-[#3F9DCF] col-span-2 row-span-3 col-start-3 row-start-5 py-[30px] pl-[15px] border-none rounded-[10px]  w-full h-full  " style="background-image: url('{{ asset ("storage/image/dom2.jpg") }}');background-position: center; w-full; h-full; background-size: cover; background-repeat: no-repeat; "">
                 <span class=" text-[14px] md:text-[16px] text-white" >#000</span>
                 <h3 class=" text-[21px] md:text-[26px] text-white font-bold">Wake Up and <br>
                 Smell the Coffee</h3>
-                <button class="mt-[50px] md:text-[#b6b6e3] bg-white px-[10px] py-[5px] md:mt-[100px]">
+                <button class="mt-[50px] md:text-[#3F9DCF] bg-white px-[20px] py-[5px] md:mt-[100px] cursor-pointer">
                     Read for $0.29
                 </button>
             </div>
